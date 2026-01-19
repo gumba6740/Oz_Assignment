98c84a81bd9a44c3b16504a089d12a469ad507e9
gumba <gumba6740@gmail.com>
2026-01-19 13:21:55 +0900

    데이터베이스 과제 0119

--- Database/BE_17_최승용_0119/읽기.sql
+++ Database/BE_17_최승용_0119/읽기.sql
@@ -5,7 +5,7 @@ SELECT name, salary FROM employees;
 
 -- Frontend 직책을 가진 직원 중에서 
 -- 연봉이 90000 이하인 직원의 이름과 연봉을 조회하세요
-SELECT name, position, salary
+SELECT name, salary
 FROM employees
 WHERE position = 'Frontend'
 AND salary <= 90000;
